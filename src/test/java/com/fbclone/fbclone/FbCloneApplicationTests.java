6bddf5cd333f22fc61c6eb75b6fab6fa04e80e41
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-02 13:47:21 -0500

    Feat: First Commit

---
+++ src/test/java/com/fbclone/fbclone/FbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fbclone.fbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
