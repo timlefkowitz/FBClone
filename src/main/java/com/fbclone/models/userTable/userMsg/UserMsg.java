a2c888c062f676b04d5d28842920ebcc5dac1c62
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-04 16:08:15 -0500

    feat: updating tables

--- src/main/java/com/fbclone/models/userTable/userMsg/UserMsg.java
+++ src/main/java/com/fbclone/models/userTable/userMsg/UserMsg.java
@@ -14,13 +14,19 @@ public class UserMsg {
     @Column(name = "UserMsg")
     private long UserMsg;
 
+    @Column(nullable = false, length = 100)
+    private long sourceId;
 
-Id
-Source Id
-Target Id
-Message
-Created At
-Updated At
+    @Column(nullable = false, length = 100)
+    private long targetId;
 
+    @Column(nullable = false, length = 250)
+    private String message;
+
+    @Column(nullable = false, length = 100)
+    private String createdAt;
+
+    @Column(nullable = false, length = 100)
+    private String updatedAt;
 
 }
