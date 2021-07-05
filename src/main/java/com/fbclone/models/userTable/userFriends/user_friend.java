c1ed627d0ddf819ccc8277da5c705c2dc11ac563
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-05 12:44:49 -0500

    feat: updating tables

--- src/main/java/com/fbclone/models/userTable/userFriends/user_friend.java
+++ src/main/java/com/fbclone/models/userTable/userFriends/user_friend.java
@@ -3,6 +3,7 @@ package com.fbclone.models.userTable.userFriends;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 
 @Entity
@@ -24,10 +25,10 @@ public class user_friend {
     private String type;
 
     @Column(nullable = false, length = 100)
-    private String createdAt;
+    private Timestamp createdAt;
 
     @Column(nullable = false, length = 100)
-    private String updatedAt;
+    private Timestamp updatedAt;
 
 
 
