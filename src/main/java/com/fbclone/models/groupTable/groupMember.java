36732c6b3bff10b6f8e786a2d92a59f4674e288a
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-08 12:04:57 -0500

    Feat: Mapping out the database

--- src/main/java/com/fbclone/models/groupTable/groupMember.java
+++ src/main/java/com/fbclone/models/groupTable/groupMember.java
@@ -3,6 +3,7 @@ package com.fbclone.models.groupTable;
 
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Entity
 
@@ -13,6 +14,28 @@ public class groupMember {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "groupMember")
     private long groupMember;
+
+    @Column(name = "groupId")
+    private long groupId;
+
+    @Column(name = "userId")
+    private long userId;
+
+    @Column(name = "roleId")
+    private String roleId;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "createdAt")
+    private Timestamp createdAt;
+
+    @Column(name = "updatedAt")
+    private Timestamp updatedAt;
+
+    @Column(name = "notes")
+    private String notes;
+
 
 
 }
