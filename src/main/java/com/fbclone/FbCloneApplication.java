0ef32a2ec4051ddf8bfb1651999e511a6e5ee6eb
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-11 02:00:17 -0500

    feat: updating columns

--- src/main/java/com/fbclone/FbCloneApplication.java
+++ src/main/java/com/fbclone/FbCloneApplication.java
@@ -1,13 +1,29 @@
 package com.fbclone;
 
+import com.fbclone.Repos.UserProfileRepo;
+import com.fbclone.Repos.UserRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FbCloneApplication {
+public class FbCloneApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FbCloneApplication.class, args);
     }
 
+
+    @Autowired
+    private UserRepos userRepos;
+
+    @Autowired
+    private UserProfileRepo userProfileRepo;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
---
+++ src/main/java/com/fbclone/Repos/UserProfileRepo.java
@@ -0,0 +1,12 @@
+package com.fbclone.Repos;
+
+
+import com.fbclone.models.userTable.userProfile;
+import com.mysql.cj.jdbc.JdbcPreparedStatement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserProfileRepo extends JpaRepository<userProfile, Long> {
+}
+
