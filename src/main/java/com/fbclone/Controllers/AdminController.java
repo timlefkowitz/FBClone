7faca9471c4a669ff7444c30e2a31cd9ae4359db
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-11 23:47:55 -0500

    feat:adding views

--- src/main/java/com/fbclone/Controllers/AdminController.java
+++ src/main/java/com/fbclone/Controllers/AdminController.java
@@ -1,12 +1,39 @@
 package com.fbclone.Controllers;
 
 
+import com.fbclone.Repos.GroupRepository;
+import com.fbclone.Repos.UserProfileRepository;
+import com.fbclone.Repos.UsersPostsRepository;
+import com.fbclone.Repos.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+
+        /*                      Repositories
+    []
+    []  lets take this repository list and make it an object?
+    []  then import the object as one line? I'm not sure how to do that =D
+    []
+    []
+     */
+
+
+    @Autowired
+    private final GroupRepository groupRepositoryDao;
+
+    @Autowired
+    private final UserProfileRepository usersProfileDao;
+
+    @Autowired
+    private final UsersPostsRepository usersPostsDao;
+
+    @Autowired
+    private final UsersRepository userDao;
 
 
 //    Admin home
