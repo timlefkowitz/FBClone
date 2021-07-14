39fbb774b1b2c12cd19e6998331c4936844812a6
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-07-14 14:17:38 -0500

    feat: updates to crushedmonkey

--- src/main/java/com/fbclone/FbCloneApplication.java
+++ src/main/java/com/fbclone/FbCloneApplication.java
@@ -1,15 +1,10 @@
 package com.fbclone;
 
-import com.fbclone.Repos.UserProfileRepository;
-import com.fbclone.Repos.UsersRepository;
-import com.fbclone.models.RelationshipHome.user;
-import com.fbclone.models.RelationshipHome.userTable.userProfile;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDate;
+
 
 @SpringBootApplication
 public class FbCloneApplication  {
